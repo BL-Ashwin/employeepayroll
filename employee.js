2b0d4f2ff81cf4325306b09f69a3ff5e67841ea6
Ashwindurairaj22 <ashwin.durairaj@bridgelabz.com>
2025-06-23 15:57:22 +0530

    uc-2 daily wage

--- employee.js
+++ employee.js
@@ -1,15 +1,51 @@
+// Employee Payroll Application
 class EmployeePayroll {
-    constructor(message) {
-        this.message = message; // store the message in a property
+  // Constructor to initialize employee details
+  constructor(empId, empName) {
+    this.empId = empId;
+    this.empName = empName;
+    this.attendance = "";
+    this.dailyWage = 0;
+  }
+ 
+  displayMessage() {
+    console.log("Welcome to Employee Payroll Application");
+  }
+  // Method to mark attendance randomly
+  markAttendance() {
+    let isPresent = Math.floor(Math.random() * 2);
+    this.attendance = isPresent ? "Present" : "Absent";
+  }
+  //UC2 - For calculating daily wage based on attendance
+  calculateWage() {
+    const WAGE_PER_HOUR = 20;
+    const WORKING_HOURS = 8;
+    const dailyWage = WAGE_PER_HOUR * WORKING_HOURS;
+    if(this.attendance === "Present") {
+      this.dailyWage = dailyWage;
+    } else {
+      this.dailyWage = 0;
     }
-
-    displayMessage() {
-        console.log(this.message); // use 'this' to access the message
-    }
+  }
+  // method to display employee details
+  displayDetails() {
+    console.log(
+      `Employee ID: ${this.empId}, Name: ${this.empName}, Attendance: ${this.attendance}, Daily Wage: ${this.dailyWage}`
+    );
+  }
 }
-
-// Create an instance with a message
-const employee = new EmployeePayroll("Welcome to employee");
-
-// Call the method to display the message
-employee.displayMessage();
+const employeePayroll = new EmployeePayroll();
+employeePayroll.displayMessage();
+//create employee objects
+let empDetails = [
+  new EmployeePayroll(11, "Ashwin"),
+  new EmployeePayroll(41, "Kural"),
+  new EmployeePayroll(12, "Deepak"),
+];
+// Mark attendance for each employee and display their details
+empDetails.forEach((employee) => {
+  employee.markAttendance();
+  employee.calculateWage(); //UC2 Implementing calculateWage method
+  employee.displayDetails();
+});
+ 
